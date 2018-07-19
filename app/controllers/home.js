b465c62ae060cf3879dff9220c5cd7b52a24f110
gleds3000 <gleds3000@gmail.com>
2018-07-19 18:48:18 -0300

    ajustes e campo email

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,7 +4,7 @@ module.exports.index = function(application, req, res){
     var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
     noticiasModel.get5UltimasNoticias(function(error, result){
-         console.log(result);
+        // console.log(result);
         res.render('home/index', { noticias: result });
     })
 
